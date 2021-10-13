871a04a6679d0235900376998b7f88240908ad68
prajapatilalit <prajapatilalit150992@gmail.com>
2021-10-13 11:51:10 +0530

    create and update book search component and provide style

--- booksearch/src/components/BookSearch.js
+++ booksearch/src/components/BookSearch.js
@@ -2,10 +2,28 @@ import React from "react";
 
 const BookSearch = () => {
   return (
-    <div class="ui search">
-      <div class="ui icon input">
-        <input class="prompt" type="text" placeholder="Common passwords..." />
-        <i class="search icon"></i>
+    <div className="container">
+      <div className="book_search">
+        <h2>Book Search</h2>
+        <p>ISBN</p>
+        <form>
+          <input type="text" placeholder="Common passwords..." />
+          <button>Search</button>
+        </form>
+        <div className="table">
+          <div className="table_col1">
+            <p>ISBN</p>
+            <p>Name</p>
+            <p>Author</p>
+            <p>Publisher</p>
+          </div>
+          <div className="table_col2">
+            <p>xxxxxxxxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxxxxxxxx</p>
+          </div>
+        </div>
       </div>
       <div class="results"></div>
     </div>
